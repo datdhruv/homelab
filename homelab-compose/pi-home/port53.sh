5984c76be527fcdd2ed1bfbc82f2304b427f76e6
Dhruv <dhruvjain1952@gmail.com>
2024-04-28 00:07:54 +0300

    Add initial services with docker compose files for each

---
+++ homelab-compose/pi-home/port53.sh
@@ -0,0 +1,3 @@
+sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+systemctl restart systemd-resolved
